6683e4aaf9fba35280ae0ce1dbab3a80f44f546a
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-20 10:23:25 +0200

    testing new thing with Sensio

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,32 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
+
     public function homepage()
     {
         return new Response('IT S WORKING !');
     }
+
+    // smash peux être remplacer par n'importe quel mot, ci dessous je print l'url !
+    /**
+     * @Route("/news/{smash}")
+     */
+    // print ce qu'il y a dans l'url sera utile par exemple pour récuperer une ID util
+    
+    public function show($smash)
+    {
+        return new Response(sprintf(
+            'Next smash incoming soon! %s',
+            $smash
+        ));
+    }
+
 }
