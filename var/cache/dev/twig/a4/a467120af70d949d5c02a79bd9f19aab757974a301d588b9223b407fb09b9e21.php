236701589ad2670f5695be4585fcdee95f3d7449
Z-dorf <sebastian.zanon2@gmail.com>
2018-06-20 15:32:55 +0200

    var stuff

--- var/cache/dev/twig/a4/a467120af70d949d5c02a79bd9f19aab757974a301d588b9223b407fb09b9e21.php
+++ var/cache/dev/twig/a4/a467120af70d949d5c02a79bd9f19aab757974a301d588b9223b407fb09b9e21.php
@@ -31,26 +31,72 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
 
         // line 1
         echo "<!DOCTYPE html>
-<html>
+<html lang=\"en\">
+
     <head>
-        <meta charset=\"UTF-8\">
         <title>";
         // line 5
         $this->displayBlock('title', $context, $blocks);
         echo "</title>
-        ";
-        // line 6
-        $this->displayBlock('stylesheets', $context, $blocks);
-        // line 7
-        echo "    </head>
-    <body>
+        <meta charset=\"utf-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+
         ";
         // line 9
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 12
+        echo "    </head>
+
+    <body>
+        <nav class=\"navbar navbar-expand-lg navbar-dark navbar-bg mb-5\">
+            <a style=\"margin-left: 75px;\" class=\"navbar-brand space-brand\" href=\"#\">The Space Bar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+            <span class=\"navbar-toggler-icon\"></span>
+          </button>
+            <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">
+                <ul class=\"navbar-nav mr-auto\">
+                     <li class=\"nav-item\">
+                       <a style=\"color: #fff;\" class=\"nav-link\" href=\"#\">Local Asteroids</a>
+                     </li>
+                     <li class=\"nav-item\">
+                       <a style=\"color: #fff;\" class=\"nav-link\" href=\"#\">Weather</a>
+                     </li>
+                   </ul>
+                <form class=\"form-inline my-2 my-lg-0\">
+                    <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+                    <button class=\"btn btn-info my-2 my-sm-0\" type=\"submit\">Search</button>
+                </form>
+                <ul class=\"navbar-nav ml-auto\">
+                    <li class=\"nav-item dropdown\" style=\"margin-right: 75px;\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"http://example.com\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                  <img class=\"nav-profile-img rounded-circle\" src=\"images/astronaut-profile.png\">
+                </a>
+                        <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdownMenuLink\">
+                            <a class=\"dropdown-item\" href=\"#\">Profile</a>
+                            <a class=\"dropdown-item\" href=\"#\">Create Post</a>
+                            <a class=\"dropdown-item\" href=\"#\">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        ";
+        // line 48
         $this->displayBlock('body', $context, $blocks);
-        // line 10
-        echo "        ";
+        // line 49
+        echo "
+        <footer class=\"footer\">
+            <div class=\"container text-center\">
+                <span class=\"text-muted\">Made with <i class=\"fa fa-heart\" style=\"color: red;\"></i> by the guys and gals at <a href=\"https://knpuniversity.com\">KnpUniversity</a></span>
+            </div>
+        </footer>
+
+
+        ";
+        // line 57
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 11
+        // line 65
         echo "    </body>
 </html>
 ";
@@ -71,24 +117,7 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
 
-        echo "Welcome!";
-        
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
-
-        
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
-
-    }
-
-    // line 6
-    public function block_stylesheets($context, array $blocks = array())
-    {
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
-
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
-
+        echo "Welcome to the SpaceBar";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -98,6 +127,26 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
     }
 
     // line 9
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 10
+        echo "            <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css\" integrity=\"sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb\" crossorigin=\"anonymous\">
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 48
     public function block_body($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -114,7 +163,7 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
 
     }
 
-    // line 10
+    // line 57
     public function block_javascripts($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -123,6 +172,14 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
 
+        // line 58
+        echo "            <script src=\"https://code.jquery.com/jquery-3.2.1.min.js\" integrity=\"sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js\" integrity=\"sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js\" integrity=\"sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ\" crossorigin=\"anonymous\"></script>
+            <script>
+                \$('.dropdown-toggle').dropdown();
+            </script>
+        ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -138,21 +195,75 @@ class __TwigTemplate_dd372c9d230818e4c0e7ad2ba3fd7818452e8cbefc6e591ccfecd950f0c
 
     public function getDebugInfo()
     {
-        return array (  118 => 10,  101 => 9,  84 => 6,  66 => 5,  54 => 11,  51 => 10,  49 => 9,  45 => 7,  43 => 6,  39 => 5,  33 => 1,);
+        return array (  176 => 58,  167 => 57,  150 => 48,  139 => 10,  130 => 9,  112 => 5,  100 => 65,  98 => 57,  88 => 49,  86 => 48,  48 => 12,  46 => 9,  39 => 5,  33 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Twig_Source("<!DOCTYPE html>
-<html>
+<html lang=\"en\">
+
     <head>
-        <meta charset=\"UTF-8\">
-        <title>{% block title %}Welcome!{% endblock %}</title>
-        {% block stylesheets %}{% endblock %}
+        <title>{% block title %}Welcome to the SpaceBar{% endblock %}</title>
+        <meta charset=\"utf-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+
+        {% block stylesheets %}
+            <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css\" integrity=\"sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb\" crossorigin=\"anonymous\">
+        {% endblock %}
     </head>
+
     <body>
+        <nav class=\"navbar navbar-expand-lg navbar-dark navbar-bg mb-5\">
+            <a style=\"margin-left: 75px;\" class=\"navbar-brand space-brand\" href=\"#\">The Space Bar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+            <span class=\"navbar-toggler-icon\"></span>
+          </button>
+            <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">
+                <ul class=\"navbar-nav mr-auto\">
+                     <li class=\"nav-item\">
+                       <a style=\"color: #fff;\" class=\"nav-link\" href=\"#\">Local Asteroids</a>
+                     </li>
+                     <li class=\"nav-item\">
+                       <a style=\"color: #fff;\" class=\"nav-link\" href=\"#\">Weather</a>
+                     </li>
+                   </ul>
+                <form class=\"form-inline my-2 my-lg-0\">
+                    <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">
+                    <button class=\"btn btn-info my-2 my-sm-0\" type=\"submit\">Search</button>
+                </form>
+                <ul class=\"navbar-nav ml-auto\">
+                    <li class=\"nav-item dropdown\" style=\"margin-right: 75px;\">
+                        <a class=\"nav-link dropdown-toggle\" href=\"http://example.com\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                  <img class=\"nav-profile-img rounded-circle\" src=\"images/astronaut-profile.png\">
+                </a>
+                        <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdownMenuLink\">
+                            <a class=\"dropdown-item\" href=\"#\">Profile</a>
+                            <a class=\"dropdown-item\" href=\"#\">Create Post</a>
+                            <a class=\"dropdown-item\" href=\"#\">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
         {% block body %}{% endblock %}
-        {% block javascripts %}{% endblock %}
+
+        <footer class=\"footer\">
+            <div class=\"container text-center\">
+                <span class=\"text-muted\">Made with <i class=\"fa fa-heart\" style=\"color: red;\"></i> by the guys and gals at <a href=\"https://knpuniversity.com\">KnpUniversity</a></span>
+            </div>
+        </footer>
+
+
+        {% block javascripts %}
+            <script src=\"https://code.jquery.com/jquery-3.2.1.min.js\" integrity=\"sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js\" integrity=\"sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh\" crossorigin=\"anonymous\"></script>
+            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js\" integrity=\"sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ\" crossorigin=\"anonymous\"></script>
+            <script>
+                \$('.dropdown-toggle').dropdown();
+            </script>
+        {% endblock %}
     </body>
 </html>
 ", "base.html.twig", "/var/www/html/the_spacebar/templates/base.html.twig");
